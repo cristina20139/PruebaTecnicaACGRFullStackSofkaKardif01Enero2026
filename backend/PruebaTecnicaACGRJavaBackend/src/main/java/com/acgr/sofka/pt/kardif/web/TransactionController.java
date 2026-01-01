74728396f112ab84c96672d6eaebb1d7b397a5c0
Aura Cristina Garzón Rodríguez <auragarzonr@gmail.com>
2026-01-01 03:25:23 -0500

    Add GET endpoint for retrieving all transactions
    This commit introduces a new GET endpoint in the TransactionController for retrieving all transactions. The new method, getAllTransactions, utilizes the transactionService to fetch a list of TransactionResponse objects. The method returns a Flux of TransactionResponse, allowing for reactive handling of multiple responses. Additionally, some minor cleanup was performed in the registerTransaction method to return the raw result from the transactionService, enhancing clarity and consistency in the code. This update enhances the API by allowing clients to access transaction data efficiently.

--- backend/PruebaTecnicaACGRJavaBackend/src/main/java/com/acgr/sofka/pt/kardif/web/TransactionController.java
+++ backend/PruebaTecnicaACGRJavaBackend/src/main/java/com/acgr/sofka/pt/kardif/web/TransactionController.java
@@ -3,6 +3,7 @@ package com.acgr.sofka.pt.kardif.web;
 import jakarta.validation.Valid;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import com.acgr.sofka.pt.kardif.service.TransactionService;
 import com.acgr.sofka.pt.kardif.service.dto.TransactionRequest;
 import com.acgr.sofka.pt.kardif.service.dto.TransactionResponse;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -25,10 +27,14 @@ public class TransactionController {
         this.transactionService = transactionService;
     }
 
+    @GetMapping
+    public Flux<TransactionResponse> getAllTransactions() {
+        return transactionService.getAllTransactions();
+    }
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Mono<TransactionResponse> registerTransaction(@Valid @RequestBody TransactionRequest request) {
-        return transactionService.registerTransaction(request.amount())
-                .map(TransactionResponse::from);
+        return transactionService.registerTransaction(request.amount());
     }
 }
