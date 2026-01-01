4ec2c7ada5172b6802b2a1c42af46ad9c19739c7
Aura Cristina Garzón Rodríguez <auragarzonr@gmail.com>
2026-01-01 00:39:59 -0500

    Add main application class for Java backend
    This commit introduces the main application class for the Prueba Tecnica ACGR Java Backend. The class, named PruebaTecnicaAcgrJavaBackendApplication, is annotated with @SpringBootApplication, which enables auto-configuration and component scanning within the application. The main method is defined to launch the Spring application using SpringApplication.run, allowing the backend service to be started. This sets the foundation for further development and integration of features within the backend application.

---
+++ backend/PruebaTecnicaACGRJavaBackend/src/main/java/com/acgr/sofka/pt/kardif/PruebaTecnicaAcgrJavaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.acgr.sofka.pt.kardif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTecnicaAcgrJavaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaTecnicaAcgrJavaBackendApplication.class, args);
+	}
+
+}
